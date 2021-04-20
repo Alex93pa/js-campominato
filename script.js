60f325e40e56518d36fc0be0658b6c39f77ad077
Alex93pa <Ale.pug2@gmail.com>
2021-04-20 12:42:12 +0200

    florian

--- script.js
+++ script.js
@@ -18,6 +18,67 @@
     var aiNumbersLenght = 16;
 
     var aiNumbers = [];
+    var userNumbers = [];
+
+    //chiede all'utente di inserire un numero tramite prompt x volte, dove x è la differenza tra il valore max number e aiNumbersLenght
+    //controllare che il numero inserito sia valido e che non sia anora stato usato
+    //deve anche controllare che il numero non esista all'interno dell'array aiNumbers
+    function askUserNumbers() {
+        var userLenghtMax = maxNumber - minNumber- aiNumbersLenght;
+
+        var gameOver = false
+
+        //finche la lenght dei num inseriti è min della lenght max dei numeri inseriti, continuo a chiedere all'utente di inserire un num tramite prompt
+        while (userNumbers.length < userLenghtMax && !gameOver) {
+            var userInput = prompt("inserisci un numero tra " + minNumber + "e " + maxNumber);
+            
+            if(userInput == null) {
+                gameOver == true;
+            }
+            if(userNumbers.length === userLenghtMax) {
+                alert("HAI VINTO!");
+                gameOver = true
+            }
+
+            // se il valore inserito è valido, ritorna true 
+            // se il valore non è valido e l'utete deve reinserirlo, ritorna false
+            // se il valore non è valido perchè è una mina, ritorna game over
+            var inputIsValid = checkUserInput(userInput)
+
+            if(!inputIsValid && inputIsValid !== "game over"){
+                alert("Numero inserito non valido");
+            } else if (inputIsValid === "game over"){
+                gameOver = true
+                alert("hai perso dopo aver inserito " + userNumbers.length + " numeri.");
+            } else {
+                userNumbers.push(parseInt(userInput));
+            }                    
+        }
+    }
+
+    //deve controllare se : -il valore è un num reale -il num NON è minore di minNumber -non èmaggiore di maxNumber -non sia già stato usato dall'urente 
+    function checkUserInput(inputValue) {
+        var result = true
+        var numberToCheck = parseInt(inputValue)
+
+        if(Number.isNaN(numberToCheck )) {
+            return false
+        }
+
+        if(numberToCheck < minNumber|| numberToCheck > maxNumber){
+            return false
+        }
+
+        if (userNumbers.indexOf(numberToCheck) > -1) {
+            return false
+        }
+
+        if(aiNumbers.indexOf(numberToCheck) > -1) {
+            return "game over";
+        }
+
+        return result;
+    }
 
     //crea i num iniziali del compter e si assicura che non siano doppi
     function createAiNumbers() {
@@ -37,7 +98,6 @@
     }
 
     createAiNumbers();
-
-
+    askUserNumbers();
 })
 ()
